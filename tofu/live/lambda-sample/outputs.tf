d70fb012faee75a2adb38d883acda6da75fdef28
Maxat Akbanov <30049303+Brain2life@users.noreply.github.com>
2024-09-04 15:04:10 +0500

    Turn off healthz output

--- tofu/live/lambda-sample/outputs.tf
+++ tofu/live/lambda-sample/outputs.tf
@@ -2,6 +2,6 @@ output "api_endpoint" {
   value = module.gateway.api_endpoint
 }
 
-output "healthz_endpoint" {
-  value = module.healthz-endpoint.api_endpoint
-}
+# output "healthz_endpoint" {
+#   value = module.healthz-endpoint.api_endpoint
+# }
