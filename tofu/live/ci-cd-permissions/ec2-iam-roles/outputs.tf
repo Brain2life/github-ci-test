57153960c34d332b3d3846a77406d1952fe95913
Maxat Akbanov <30049303+Brain2life@users.noreply.github.com>
2024-09-08 22:09:49 +0500

    Add CI CD permissions for EC2 plan

---
+++ tofu/live/ci-cd-permissions/ec2-iam-roles/outputs.tf
@@ -0,0 +1,7 @@
+output "ec2_deploy_plan_role_arn" {
+  value = module.iam_roles.ec2_deploy_plan_role_arn
+}
+
+# output "lambda_deploy_apply_role_arn" {
+#   value = module.iam_roles.lambda_deploy_apply_role_arn
+# }
